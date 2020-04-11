a83723fcec6160c653d531035842aae78a863244
LCH2HK <614713250@qq.com>
2020-04-11 10:52:06 +0800

    1212

--- src/main/java/链表/求两个链表的交点.java
+++ src/main/java/链表/求两个链表的交点.java
@@ -6,10 +6,9 @@ import java.sql.SQLOutput;
  * 已知链表A的头节点指针headA，链表B的头节点指针headB，两个链表相交，求两个链表交点对应的节点
  * @author luch
  * @date 2020-4-11-9:55
- 11111111111
  */
 public class 求两个链表的交点 {
     public static void main(String[] args) {
-        
+        sys
     }
 }
